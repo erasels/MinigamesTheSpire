73435eaa9ad2ff01e67e77b70016418a2a4bc5ee
Alchyr <isithalchyr@gmail.com>
2021-01-13 08:54:35 -0800

    reset in a smarter place (for save/quit shenangnigans)

--- MinigamesTheSpire/src/main/java/Minigames/events/AbstractMinigameEvent.java
+++ MinigamesTheSpire/src/main/java/Minigames/events/AbstractMinigameEvent.java
@@ -11,17 +11,17 @@ public abstract class AbstractMinigameEvent extends AbstractImageEvent {
 
     public AbstractMinigameEvent(String title, String body, String imgUrl) {
         super(title, body, imgUrl);
+
+        if (game != null)
+        {
+            game.dispose(); //player quit in middle of a minigame, dispose the old one.
+        }
     }
 
     protected void startGame(AbstractMinigame newGame)
     {
         this.imageEventText.clearAllDialogs();
         GenericEventDialog.hide();
-
-        if (game != null)
-        {
-            game.dispose(); //player quit in middle of a minigame, dispose the old one.
-        }
 
         game = newGame;
         game.initialize();
