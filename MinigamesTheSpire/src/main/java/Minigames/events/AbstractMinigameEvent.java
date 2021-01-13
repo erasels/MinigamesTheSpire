0b31d99afb6f75c8ae894fea459600995ffb6dfc
erasels <emanuelbedburdick@yahoo.de>
2021-01-13 12:23:27 +0100

    Update AbstractMinigameEvent.java

--- MinigamesTheSpire/src/main/java/Minigames/events/AbstractMinigameEvent.java
+++ MinigamesTheSpire/src/main/java/Minigames/events/AbstractMinigameEvent.java
@@ -2,13 +2,8 @@ package Minigames.events;
 
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 
-public class AbstractMinigameEvent extends AbstractImageEvent {
+public abstract class AbstractMinigameEvent extends AbstractImageEvent {
     public AbstractMinigameEvent(String title, String body, String imgUrl) {
         super(title, body, imgUrl);
-    }
-
-    @Override
-    protected void buttonEffect(int i) {
-
     }
 }
