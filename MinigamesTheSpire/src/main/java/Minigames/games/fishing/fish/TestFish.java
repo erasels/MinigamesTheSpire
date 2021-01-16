e47c05b87cad58ff45ad496165762f22932c2ba1
erasels <emanuelbedburdick@yahoo.de>
2021-01-16 21:32:17 +0100

    Update TestFish.java

--- MinigamesTheSpire/src/main/java/Minigames/games/fishing/fish/TestFish.java
+++ MinigamesTheSpire/src/main/java/Minigames/games/fishing/fish/TestFish.java
@@ -1,12 +1,13 @@
 package Minigames.games.fishing.fish;
 
 import com.badlogic.gdx.math.Vector2;
+import com.megacrit.cardcrawl.rewards.RewardItem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestFish extends AbstractFish {
-    public static final float MAX_HP = 50f;
+    public static final float MAX_HP = 10f;
     private static final ArrayList<Vector2> BEHAVIORS = new ArrayList<>(Arrays.asList(new Vector2(10f, 0), new Vector2(2f, 1f)));
     public TestFish() {
         super(MAX_HP, BEHAVIORS);
@@ -16,7 +17,12 @@ public class TestFish extends AbstractFish {
     public void update(boolean inArea) {
         super.update(inArea);
         if(inArea) {
-            System.out.println(hp);
+            //System.out.println(hp);
         }
+    }
+
+    @Override
+    public ArrayList<RewardItem> returnReward() {
+        return new ArrayList<>(Arrays.asList(new RewardItem(75)));
     }
 }
