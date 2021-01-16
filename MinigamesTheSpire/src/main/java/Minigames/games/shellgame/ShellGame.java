dd29c0e282f5f92b78a52b7da1cedabbfa2f2e80
Vex Darkon <edanvirgo@gmail.com>
2021-01-15 23:06:26 -0700

    Game plan (literally)

--- MinigamesTheSpire/src/main/java/Minigames/games/shellgame/ShellGame.java
+++ MinigamesTheSpire/src/main/java/Minigames/games/shellgame/ShellGame.java
@@ -5,6 +5,19 @@ import Minigames.games.input.bindings.BindingGroup;
 
 public class ShellGame extends AbstractMinigame {
 
+    /*
+    So, basically the way this should be coded, I think
+    is that we have the 3 shell objects.
+    We initialize the rewards on wherever this stuff inits,
+    then initialize the shells with those rewards.
+    Phase is 0 on setup, which is where we see the rewards, and then the shell x/y move to cover them.
+    Then we do phase 1, which is the shuffling part. You watch them shuffle
+    and then once they're done shuffling, we make it clear you can click,
+    and then that's phase 2. On phase 2, when you click,
+    pull up the corresponding shell, and grant the reward in a pretty fashion,
+    like how Gremlin Match puts the cards in your deck from the screen. Boom!
+     */
+
     private Shell shell1;
     private Shell shell2;
     private Shell shell3;
@@ -13,18 +26,18 @@ public class ShellGame extends AbstractMinigame {
 
     private void onClick() {
         switch (phase) {
-            case 1:
+            case 2:
                 if (shell1.hb.hovered) {
                     chosen = 1;
-                    phase = 2;
+                    phase = 3;
                 }
                 else if (shell2.hb.hovered) {
                     chosen = 2;
-                    phase = 2;
+                    phase = 3;
                 }
                 else if (shell3.hb.hovered) {
                     chosen = 3;
-                    phase = 2;
+                    phase = 3;
                 }
         }
     }
