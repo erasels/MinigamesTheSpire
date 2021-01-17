36cad056231fddb63070d3854a2119839f6647ef
Vex Darkon <edanvirgo@gmail.com>
2021-01-16 18:56:06 -0700

    Okay, it works

--- MinigamesTheSpire/src/main/java/Minigames/patches/PlayMusicGoodPatch.java
+++ MinigamesTheSpire/src/main/java/Minigames/patches/PlayMusicGoodPatch.java
@@ -3,18 +3,29 @@ package Minigames.patches;
 import com.badlogic.gdx.audio.Music;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.audio.MainMusic;
 import com.megacrit.cardcrawl.audio.TempMusic;
 
 import static Minigames.Minigames.makeAudioPath;
 
-@SpirePatch(clz = TempMusic.class, method = "getSong")
+@SpirePatch(
+        clz = TempMusic.class,
+        method = "getSong")
 public class PlayMusicGoodPatch {
     @SpirePostfixPatch
-    public static Music Postfix(Music __result, TempMusic __instance, String key) {
-        if ("minigames:carnivalMusic".equals(key)) {
-            return MainMusic.newMusic(makeAudioPath("carnivalMusic.mp3"));
+    public static SpireReturn<Music> Prefix(TempMusic __instance, String key) {
+        System.out.println("YOU ACTUALLY HIT THE PATCH GOOD JOB, PAST VEX! key: " + key);
+        switch (key) {
+            case "minigames:carnivalMusic": {
+                System.out.println("YES, IT LIKES THE KEY!");
+                return SpireReturn.Return(MainMusic.newMusic(makeAudioPath("carnivalMusic.mp3")));
+            }
+            default: {
+                System.out.println("NO, IT HATES THE KEY!");
+                return SpireReturn.Continue();
+            }
         }
-        return __result;
     }
 }
+
