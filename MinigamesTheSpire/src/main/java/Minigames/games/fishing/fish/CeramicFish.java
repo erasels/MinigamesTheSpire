c4fa9ab4ccd8f44e86456492d1f7109e83110b26
erasels <emanuelbedburdick@yahoo.de>
2021-01-17 16:10:01 +0100

    Reverted test value

--- MinigamesTheSpire/src/main/java/Minigames/games/fishing/fish/CeramicFish.java
+++ MinigamesTheSpire/src/main/java/Minigames/games/fishing/fish/CeramicFish.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CeramicFish extends AbstractFish{
-    public static final float MAX_HP = 0.005f;
+    public static final float MAX_HP = 0.65f;
     private static final ArrayList<Vector2> BEHAVIORS = new ArrayList<>(Arrays.asList(
             new Vector2(5f, 0.3f),
             new Vector2(2f, 1f),
